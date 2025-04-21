ffd19296c4043966bf272f9986bcf248afa34c67
Abdullah-Kamel <abdullahkamel.750@gmail.com>
2025-04-21 23:34:39 +0200

    Fix import path casing

--- src/Components/Auth/ForgetPassword.jsx
+++ src/Components/Auth/ForgetPassword.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { PulseLoader } from "react-spinners";
-import { authService } from "../../services/AuthService";
+import { authService } from "../../Services/authService";
 import { useForgotPasswordForm } from "../../hooks/useForgotPasswordForm";
 import FormInput from "../common/FormInput";
 import Navbar from "../NavBar/NavBar";
