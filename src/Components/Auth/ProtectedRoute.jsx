fce87f8c1927dd5d9f8fde03a1176d954df6b0be
Abdullah-Kamel <abdullahkamel.750@gmail.com>
2025-05-09 21:47:30 +0300

    Fixed auth problem

--- src/Components/Auth/ProtectedRoute.jsx
+++ src/Components/Auth/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
 // src/routes/ProtectedRoute.jsx
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
+import { PulseLoader } from "react-spinners";
 
 const ProtectedRoute = ({ children }) => {
   const { user, accessToken } = useAuth();
   
   if (user === undefined) {
-    return "Loading...";
+    return (  <div className="position-absolute bg-main-light top-0 end-0 bottom-0 start-0 d-flex justify-content-center align-items-center w-100 vh-100">
+      <PulseLoader color="#0aad0a" size={30} />
+    </div>);
   }
   if (!user || !accessToken) {
     return <Navigate to="/login" replace />;
