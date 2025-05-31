387a3ed2b52d6c042ea053d052e41debaaf2b0f3
Abdulah-Kamel <abdullahkamel.750@gmai.com>
2025-05-31 22:20:11 +0300

    hide the shdule task until backend feature is done

--- src/Components/main/ContentTracker/forms/TaskForm.jsx
+++ src/Components/main/ContentTracker/forms/TaskForm.jsx
@@ -19,7 +19,7 @@ const TaskForm = ({ isCreateMode, loading, apiErrors, onHide }) => {
       {isCreateMode ? (
         <>
           <CreateTaskFields apiErrors={apiErrors} />
-          <ScheduleToggle apiErrors={apiErrors} />
+          {/* <ScheduleToggle apiErrors={apiErrors} /> */}
           {values.is_scheduled && <ScheduleFields apiErrors={apiErrors} />}
         </>
       ) : (
